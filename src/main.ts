2fbb4e7f8435513df1eb356fcaa6ed7c99d2a650
Duong Dung <dungdcth@gmail.com>
2023-09-20 22:13:07 +0700

    Add docker file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<string>('PORT');
+  await app.listen(PORT);
+  Logger.log(`🚀 ~ App running - port:${PORT}`, 'NestApplication');
 }
 bootstrap();
