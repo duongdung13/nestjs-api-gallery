b6b7eba03e280cdc7e073f3bd019950a5ef19af1
DungDC <dungdcth@gmail.com>
2023-09-22 17:43:11 +0700

    add decorator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  //config CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+
+
   const configService = app.get(ConfigService);
   const PORT = configService.get<string>('PORT');
   await app.listen(PORT);
