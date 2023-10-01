aa38adad9ae972ddaabc3497290deebd87598e9f
Duong Dung <dungdcth@gmail.com>
2023-10-01 23:18:40 +0700

    Fix crawl data

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
 import { Image, ImageSchema } from './schemas/image.schema';
+import { SettingsModule } from 'src/settings/settings.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Image.name, schema: ImageSchema }]),
+    SettingsModule,
   ],
   controllers: [ImagesController],
   providers: [ImagesService],
